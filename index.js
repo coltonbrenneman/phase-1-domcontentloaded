359b6780782fb6398b175b411feff8cd5680fee2
Colton Brenneman <colton.brenneman@gmail.com>
2022-08-15 16:29:27 -0600

    commit tree 6558d7545725f16f53e1b97bf9e4c42478aff1b5

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded",
+// function(){
+//     console.log("Them DOM has loaded");
+// });
+
+// console.log("THis console.log() fires when idnex.js loads before DOMContentLoaded is triggered");
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('text').innerHTML = "This is really cool!"
+})
